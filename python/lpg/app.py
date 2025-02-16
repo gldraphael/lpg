b1ae91cbfa05d9e78925f69d6da25f1c173b178d
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-02-16 13:39:01 +0000

    Python: Configure swagger UI (#19)

--- python/lpg/app.py
+++ python/lpg/app.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title= 'Language Playground: Python',
+    docs_url= '/',
+    openapi_url= '/docs/openapi.json'
+)
 
-
-@app.get("/hello")
+@app.get("/hello", tags=["exp"])
 def hello():
     return {"msg": "Hello, world!"}
 
-@app.post("/ping")
+@app.post("/ping", tags=["exp"])
 def ping():
     return {"msg": "pong"}
 
