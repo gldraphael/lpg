f31002ff274dc21c99f230511094debd5d7d4df6
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-02-17 12:00:13 +0000

    C#: configure openapi doc & swagger UI (#20)

--- csharp/LpgWebApp/Program.cs
+++ csharp/LpgWebApp/Program.cs
@@ -1,14 +1,29 @@
 var builder = WebApplication.CreateSlimBuilder(args);
 
-builder.Services.AddOpenApi();
+const string DOC_TITLE = "Language Playground: C#";
+
+builder.Services.AddOpenApi("openapi", options => {
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info.Title = DOC_TITLE;
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
-app.MapOpenApi();
+app.MapOpenApi(pattern: "/docs/{documentName}.json");
+app.UseSwaggerUI(options => {
+    options.DocumentTitle = DOC_TITLE;
+    options.RoutePrefix = string.Empty;
+    options.SwaggerEndpoint("/docs/openapi.json", "v1");
+});
 
 app.MapGet("/hello", () => Results.Ok(new {msg = "Hello, world!"}))
-    .WithName("hello");
+    .WithName("hello")
+    .WithTags("exp");
 app.MapPost("/ping", () => Results.Ok(new {msg = "pong"}))
-    .WithName("ping");
+    .WithName("ping")
+    .WithTags("exp");;
 
 app.Run();
